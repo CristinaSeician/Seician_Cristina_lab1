ede8622970f4320ad5f218be0a7ccea355ce6af5
CristinaSeician <cristinaseician@yahoo.com>
2025-11-24 18:56:55 +0200

    Add project files.

---
+++ Seician_Cristina_lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Seician_Cristina_lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
