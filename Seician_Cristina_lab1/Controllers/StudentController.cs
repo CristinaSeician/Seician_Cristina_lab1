cd80125456c0364aa0c51b2a68fc34ca8dec2a37
CristinaSeician <cristinaseician@yahoo.com>
2025-11-25 08:26:01 +0200

    lab1TOT

---
+++ Seician_Cristina_lab1/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seician_Cristina_lab1.Models;
+
+namespace Seician_Cristina_lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Info()
+        {
+            var student = new Student { Name = "Ana", Age = 21 };
+            return View(student);
+        }
+        public IActionResult Mesaj() 
+        {
+            ViewBag.Message = "Mesaj din Controller";
+            ViewData["Time"] = DateTime.Now;
+            return View();
+        }
+    }
+}
